a9bba7a2d2481816456138bc95c2321d58b0cd0a
estebancardoso96 <esteban.cardoso96@gmail.com>
2024-08-27 22:23:54 -0300

    commit

--- script_python.py
+++ script_python.py
@@ -1,4 +1,11 @@
-Contenido del archivo
+# Función limpieza
 
-# segundo commit
+import unicodedata
+import re
+import pandas as pd
+
+def limpiar_texto(texto):
+    texto = unicodedata.normalize('NFKD', texto).encode('ascii', 'ignore').decode('utf-8')
+    texto = re.sub(r'[^a-zA-Z0-9\s]', '', texto)
+    return texto
 
